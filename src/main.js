955b4f6df30fa2a98b16153f122e72ac9add71c7
MongieLee <mongielee@163.com>
2020-07-12 01:48:18 +0800

    封装自定义Vue时间格式插件

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import friendlyDate from '@/helpers/util.js'
 Vue.use(ElementUI)
+Vue.use(friendlyDate)
+
 Vue.config.productionTip = false
 
 new Vue({
